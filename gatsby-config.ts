8e9562159e2e3164d54dc8b5270a547c0f835f15
lim0n3 <simonestaffa96@gmail.com>
2023-12-22 20:05:51 +0100

    add icon and site title

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,11 @@ const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: "automatic",
   pathPrefix: "/farcaster-snap-site",
+  siteMetadata: {
+    title: "Farcaster Snap",
+    description: "Farcaster Snap is a MetaMask snap integrated with Farcaster social graph.",
+    siteUrl: "https://limone-eth.github.io/farcaster-snap-site",
+  },
   plugins: [
     "gatsby-plugin-svgr",
     "gatsby-plugin-styled-components",
@@ -12,6 +17,7 @@ const config: GatsbyConfig = {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: "Farcaster Snap",
+      
         icon: "src/assets/logo.svg",
         /* eslint-disable @typescript-eslint/naming-convention */
         theme_color: "#6F4CFF",
